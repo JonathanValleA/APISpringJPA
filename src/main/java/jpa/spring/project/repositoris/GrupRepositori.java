5adb7add58bf9502de892e984b44a03576d8d21e
jgimenezbalmes <jgimenez@jaumebalmes.net>
2022-10-26 17:13:28 +0200

    retocs finals a repositori grup

--- src/main/java/jpa/spring/project/repositoris/GrupRepositori.java
+++ src/main/java/jpa/spring/project/repositoris/GrupRepositori.java
@@ -1,9 +1,14 @@
 package jpa.spring.project.repositoris;
 
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.util.MultiValueMap;
 
 import jpa.spring.project.entitats.Grup;
 
-public interface GrupRepositori extends CrudRepository<Grup, Long> {
+public interface GrupRepositori extends CrudRepository<Grup, Long>{
 
 }
+
